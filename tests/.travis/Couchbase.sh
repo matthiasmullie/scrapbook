f7e52cb8920bb453bd3799a48297dc1ede7a6f93
Matthias Mullie <git@mullie.eu>
2015-09-03 18:04:06 +0200

    Go back to old Couchbase version & clarify comments

--- tests/.travis/Couchbase.sh
+++ tests/.travis/Couchbase.sh
@@ -2,9 +2,15 @@ sudo wget -O/etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com
 sudo wget -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install libcouchbase2-libevent libcouchbase-dev libssl0.9.8
-sudo wget http://packages.couchbase.com/releases/3.1.0/couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
-sudo INSTALL_DONT_START_SERVER=1 dpkg -i couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
+# I'm going to install a pretty old couchbase version with bucket-type=memcached (instead of couchbase)
+# because that one supports flush() by default. bucket-type=couchbase doesn't, and that version does
+# not yet let you --enable-flush (command unknown) when creating the bucket.
+# Reasons I'm using this old version:
+# * other 2.x versions time out trying to start the server
+# * 3.x versions can't connect to server, once installed
+sudo wget http://packages.couchbase.com/releases/2.0.1/couchbase-server-enterprise_x86_64_2.0.1.deb
+sudo dpkg -i couchbase-server-enterprise_x86_64_2.0.1.deb
 sudo service couchbase-server start
-/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8080 --cluster-init-ramsize=300
-/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u Administrator -p password --bucket=default --bucket-password= --bucket-type=couchbase --bucket-port=11222 --bucket-ramsize=100 --bucket-replica=1 --enable-flush=1 --force
+/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 -u Administrator -p password --cluster-init-ramsize=1024
+/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u Administrator -p password --bucket=default --bucket-password= --bucket-type=memcached --bucket-port=11211 --bucket-ramsize=100 --bucket-replica=0 #--enable-flush=1
 pecl install couchbase
