a7f6d5213cc952a4641ba52f0082473b134d14ce
Matthias Mullie <git@mullie.eu>
2015-09-03 17:00:10 +0200

    Install recent Couchbase version

--- tests/.travis/Couchbase.sh
+++ tests/.travis/Couchbase.sh
@@ -3,8 +3,8 @@ sudo wget -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key 
 sudo apt-get update
 sudo apt-get install libcouchbase2-libevent libcouchbase-dev libssl0.9.8
 sudo wget http://packages.couchbase.com/releases/3.1.0/couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
-sudo dpkg -i couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
+sudo INSTALL_DONT_START_SERVER=1 dpkg -i couchbase-server-enterprise_3.1.0-ubuntu12.04_amd64.deb
 sudo service couchbase-server start
-/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 -u Administrator -p password --cluster-init-ramsize=1024
-/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u Administrator -p password --bucket=default --bucket-password= --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=100 --bucket-replica=0 --enable-flush=1
+/opt/couchbase/bin/couchbase-cli cluster-init -c 127.0.0.1:8091 --cluster-init-username=Administrator --cluster-init-password=password --cluster-init-port=8080 --cluster-init-ramsize=300
+/opt/couchbase/bin/couchbase-cli bucket-create -c 127.0.0.1:8091 -u Administrator -p password --bucket=default --bucket-password= --bucket-type=couchbase --bucket-port=11222 --bucket-ramsize=100 --bucket-replica=1 --enable-flush=1 --force
 pecl install couchbase
