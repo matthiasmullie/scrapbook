a66927bf350a2689a6ebea53125830945cde5ca4
Matthias Mullie <git@mullie.eu>
2015-10-21 09:40:12 +0200

    Don't require-dev league\flysystem
    Let's just manually require it, just for those specific tests that
    need it. Otherwise:
    * Travis sets it up for tests where we don't need it, and
    * Travis fails PHP5.3 tests for other adapters as well
    
    Since Flysystem only supports PHP>=5.4, we can exclude 5.3 from
    the matrix.

--- tests/.travis/Flysystem.sh
+++ tests/.travis/Flysystem.sh
@@ -1,1 +1,5 @@
 mkdir /tmp/cache
+
+# not adding to composer.json immediately, as that also makes tests for other
+# adapters fail on 5.3, even though we don't need flysystem there
+composer require league/flysystem
