9843289bc77cea81874934bd88ed3121e84d08ac
Matthias Mullie <git@mullie.eu>
2015-11-04 08:30:21 +0000

    Normalize token result for hhvm/memcached

--- src/Adapters/Memcached.php
+++ src/Adapters/Memcached.php
@@ -63,6 +63,7 @@ class Memcached implements KeyValueStore
 
         // HHVMs getMulti() returns null instead of empty array for no results,
         // so normalize that
+        $tokens = $tokens ?: array();
         return $return ?: array();
     }
 
