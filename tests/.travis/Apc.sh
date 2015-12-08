1d462309eb94d5138c780b45aa8987f9aed56525
Matthias Mullie <git@mullie.eu>
2015-12-08 12:34:43 +0100

    Fix APC test setup

--- tests/.travis/Apc.sh
+++ tests/.travis/Apc.sh
@@ -1,19 +1,22 @@
-if [[ "$TRAVIS_PHP_VERSION" != "hhvm" ]]
-then
-    INI_PATH=~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+INI_PATH=`php -r "echo php_ini_loaded_file();"`
+PHP_VERSION=`php -r "echo phpversion();"`
 
+if [[ $PHP_VERSION != *"hhvm" ]]
+then
     if [[ `php-config --vernum` -ge 70000 ]] # PHP>=7.0
     then
         pecl config-set preferred_state beta
         printf "yes\n" | pecl install apcu
+        echo 'extension="apcu.so"' >> $INI_PATH
     elif [[ `php-config --vernum` -ge 50500 ]] # 7.0>PHP>=5.5
     then
         printf "yes\n" | pecl install apcu-4.0.8
+        echo 'extension="apcu.so"' >> $INI_PATH
     else # PHP<5.5
+        printf "yes\n" | pecl install apc
         echo 'extension="apc.so"' >> $INI_PATH
     fi
 else
-    INI_PATH=/etc/hhvm/php.ini
     echo 'extension="apc.so"' >> $INI_PATH
 fi
 
