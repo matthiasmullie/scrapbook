861ed90c4e2e0cf4a0d8b962b85da0fdff323d6c
Matthias Mullie <git@mullie.eu>
2015-12-23 09:07:29 +0100

    Fail tests where adapter can't be initialized
    PHPUnit doesn't exist with failure status code for incomplete tests.
    Also, I'm incorrectly using "incomplete" here - it's meant to surface
    tests that still need to be expanded, not unsatisfied preconditions
    (missing adapter)

--- tests/Adapters/AdapterStub.php
+++ tests/Adapters/AdapterStub.php
@@ -34,66 +34,66 @@ class AdapterStub implements KeyValueStore
 
     public function get($key, &$token = null)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function getMulti(array $keys, array &$tokens = null)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function set($key, $value, $expire = 0)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function setMulti(array $items, $expire = 0)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function delete($key)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function deleteMulti(array $keys)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function add($key, $value, $expire = 0)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function replace($key, $value, $expire = 0)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function cas($token, $key, $value, $expire = 0)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function increment($key, $offset = 1, $initial = 0, $expire = 0)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function decrement($key, $offset = 1, $initial = 0, $expire = 0)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function touch($key, $expire)
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 
     public function flush()
     {
-        $this->test->markTestIncomplete($this->exception->getMessage());
+        throw $this->exception;
     }
 }
