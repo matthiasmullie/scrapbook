d007e3388b4b7b2909586840c4dc82e4af95313e
Matthias Mullie <git@mullie.eu>
2015-12-23 09:47:13 +0100

    Run MySQL tests on default port

--- tests/.travis/MySQL.sh
+++ tests/.travis/MySQL.sh
@@ -1,1 +1,2 @@
-docker run -d -p 3305:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=cache mysql
+sudo kill -9 `sudo lsof -t -i:3306` # kill listeners on required port
+docker run -d -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=cache mysql
