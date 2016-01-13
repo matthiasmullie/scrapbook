f50c1c6b612cf17ca5b85825b2e6a1f6affbba50
Matthias Mullie <git@mullie.eu>
2016-01-13 02:26:09 -0800

    Proper integration for adapter-specific integration tests

--- tests/Psr6/Integration/IntegrationPoolTest.php
+++ tests/Psr6/Integration/IntegrationPoolTest.php
@@ -4,69 +4,73 @@ namespace MatthiasMullie\Scrapbook\Tests\Psr6\Integration;
 
 use MatthiasMullie\Scrapbook\Tests\AdapterProviderTestCase;
 use MatthiasMullie\Scrapbook\KeyValueStore;
-use MatthiasMullie\Scrapbook\Psr6\Pool;
+use PHPUnit_Framework_TestSuite;
 
 class IntegrationPoolTest extends AdapterProviderTestCase
 {
-    public function adapterProvider()
+    public static function suite()
     {
-        return array_map(function (KeyValueStore $adapter) {
-            $pool = new Pool($adapter);
+        $suite = new PHPUnit_Framework_TestSuite('Test integration');
 
-            return array($adapter, $pool);
-        }, $this->getAdapters());
+        $self = new static;
+        $i = 0;
+        foreach ($self->getAdapters() as $name => $adapter) {
+            // let's add all of the integrations tests for every adapter
+            $suite->addTestSuite('MatthiasMullie\\Scrapbook\\Tests\\Psr6\\Integration\\IntegrationPoolTestIndividual');
+
+            // we can't use --filter to narrow down on specific adapters
+            // (because we're not using dataProvider), but we can make sure it's
+            // properly split up into groups & then use --group
+            $tests = $suite->testAt($i);
+            static::injectGroup($tests, $name);
+
+            // and let's make sure to inject the specific adapter into the test
+            static::injectAdapter($tests, $adapter);
+
+            $i++;
+        }
+
+        return $suite;
     }
 
     /**
-     * @dataProvider adapterProvider
+     * Injecting an adapter must be done recursively: there are some methods
+     * that get input from dataProviders, so they're wrapped in another class
+     * that we must unwrap in order to assign the adapter.
+     *
+     * @param \PHPUnit_Framework_TestSuite $suite
+     * @param KeyValueStore $adapter
      */
-    public function testIntegration(KeyValueStore $cache, Pool $pool)
+    protected static function injectAdapter(\PHPUnit_Framework_TestSuite $suite, KeyValueStore $adapter)
     {
-        /*
-         * So I want to run all IntegrationPoolTestIndividual tests for all
-         * possible adapters (similar to all other tests), but it's an external
-         * test suite that I can not just alter & add the dataProvider to.
-         * Instead, I'll figure out what all tests in that suite are & manually
-         * run them, injecting the data & dataName data in there.
-         */
-        $dataProperty = new \ReflectionProperty('PHPUnit_Framework_TestCase', 'data');
-        $dataProperty->setAccessible(true);
-        $dataNameProperty = new \ReflectionProperty('PHPUnit_Framework_TestCase', 'dataName');
-        $dataNameProperty->setAccessible(true);
-
-        $adapters = $this->adapterProvider();
-
-        // these are the actual data & dataName that will go into the tests:
-        // the adapter & pool and the name of the adapter
-        $data = func_get_args();
-        $dataName = array_search(func_get_args(), $adapters);
-
-        // this TestSuite instance will let me iterate all the tests in it
-        $suite = new \PHPUnit_Framework_TestSuite(
-            new \ReflectionClass('MatthiasMullie\\Scrapbook\\Tests\\Psr6\\Integration\\IntegrationPoolTestIndividual')
-        );
-
-        /*
-         * I need a way to inject the pool into the tests & there's little I can
-         * do besides inject it into the result that will be passed along to the
-         * tests. I can't pass it into the tests directly (like data & dataName)
-         * because some of the tests are not instances of the test directly, but
-         * TestSuite_DataProvider instances, which will in turn spawn the tests
-         * with their dependencies.
-         */
-        $result = $this->getTestResultObject();
-        $result->pool = $pool;
-
-        /** @var IntegrationPoolTestIndividual $test */
-        foreach ($suite as $test) {
-            $dataProperty->setValue($test, $data);
-            $dataNameProperty->setValue($test, $dataName);
-
-            // run the test!
-            $test->run($result);
+        foreach ($suite as $test)
+        {
+            if ($test instanceof \PHPUnit_Framework_TestSuite) {
+                static::injectAdapter($test, $adapter);
+            } else {
+                $test->setAdapter($adapter);
+            }
         }
+    }
 
-        // undo the hack of injecting the pool object
-        unset($result->pool);
+    /**
+     * Because some tests are wrapped inside a dataProvider suite, we need to
+     * make sure that the groups are recursively assigned to each suite until we
+     * reach the child.
+     *
+     * @param PHPUnit_Framework_TestSuite $suite
+     * @param string$group
+     */
+    protected static function injectGroup(\PHPUnit_Framework_TestSuite $suite, $group)
+    {
+        $tests = $suite->tests();
+        $suite->setGroupDetails(array('default' => $tests, $group => $tests));
+
+        foreach ($suite->tests() as $test)
+        {
+            if ($test instanceof \PHPUnit_Framework_TestSuite) {
+                static::injectGroup($test, $group);
+            }
+        }
     }
 }
--- tests/Psr6/Integration/IntegrationPoolTestIndividual.php
+++ tests/Psr6/Integration/IntegrationPoolTestIndividual.php
@@ -3,15 +3,29 @@
 namespace MatthiasMullie\Scrapbook\Tests\Psr6\Integration;
 
 use Cache\IntegrationTests\CachePoolTest;
+use MatthiasMullie\Scrapbook\KeyValueStore;
 use MatthiasMullie\Scrapbook\Psr6\Pool;
 
 class IntegrationPoolTestIndividual extends CachePoolTest
 {
     /**
+     * @var KeyValueStore
+     */
+    protected $adapter;
+
+    /**
+     * @param KeyValueStore $adapter
+     */
+    public function setAdapter(KeyValueStore $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    /**
      * @return Pool
      */
     public function createCachePool()
     {
-        return $this->getTestResultObject()->pool;
+        return new Pool($this->adapter);
     }
 }
