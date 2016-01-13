6728b6723fa2e83ceca3785dd1523af4999f9f64
Matthias Mullie <git@mullie.eu>
2016-01-13 09:51:56 -0800

    Increase RAM size available for Couchbase

--- tests/.travis/Couchbase.sh
+++ tests/.travis/Couchbase.sh
@@ -13,8 +13,8 @@ untilsuccessful() {
 sudo kill -9 `sudo lsof -t -i:8091,11210,11211` # kill listeners on required ports
 docker run -d -p 8091:8091 -p 11210:11210 -p 11211:11211 couchbase
 ID=`docker ps -l -q` # last container's id
-untilsuccessful docker exec -it $ID /opt/couchbase/bin/couchbase-cli cluster-init -c localhost:8091 -u Administrator -p password --cluster-ramsize=300
-untilsuccessful docker exec -it $ID /opt/couchbase/bin/couchbase-cli bucket-create -c localhost:8091 -u Administrator -p password --bucket=default --bucket-password= --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=200 --bucket-replica=0 --enable-flush=1 --wait
+untilsuccessful docker exec -it $ID /opt/couchbase/bin/couchbase-cli cluster-init -c localhost:8091 -u Administrator -p password --cluster-ramsize=2000
+untilsuccessful docker exec -it $ID /opt/couchbase/bin/couchbase-cli bucket-create -c localhost:8091 -u Administrator -p password --bucket=default --bucket-password= --bucket-type=couchbase --bucket-port=11211 --bucket-ramsize=1500 --bucket-replica=0 --enable-flush=1 --wait
 
 sudo wget -O/etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com/ubuntu/couchbase-ubuntu1204.list
 sudo wget -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key add -
