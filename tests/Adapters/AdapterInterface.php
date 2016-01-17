89c1a5665bceeaf26f78d3f6055dbed74dc5f480
Matthias Mullie <git@mullie.eu>
2016-01-16 21:40:35 -0800

    Clarify that AdapterInterface::get should throw an Exception

--- tests/Adapters/AdapterInterface.php
+++ tests/Adapters/AdapterInterface.php
@@ -3,11 +3,14 @@
 namespace MatthiasMullie\Scrapbook\Tests\Adapters;
 
 use MatthiasMullie\Scrapbook\KeyValueStore;
+use MatthiasMullie\Scrapbook\Exception\Exception;
 
 interface AdapterInterface
 {
     /**
      * @return KeyValueStore
+     *
+     * @throws Exception
      */
     public function get();
 }
