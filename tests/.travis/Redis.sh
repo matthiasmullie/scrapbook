90419f6a0bd5b89b70b9b613bd3b156b4e76c261
Matthias Mullie <git@mullie.eu>
2016-07-04 13:52:55 +0200

    Fix some test setups

--- tests/.travis/Redis.sh
+++ tests/.travis/Redis.sh
@@ -3,6 +3,11 @@ INI_PATH=`php -r "echo php_ini_loaded_file();"`
 sudo kill -9 `sudo lsof -t -i:6379` # kill listeners on required port
 docker run -d -p 6379:6379 redis
 
-pecl install redis
+if [[ `php-config --vernum` -ge 70000 ]] # PHP>=7.0
+then
+    pecl install redis
+else
+    pecl install redis-2.2.7
+fi
 
 echo 'extension="redis.so"' >> $INI_PATH
