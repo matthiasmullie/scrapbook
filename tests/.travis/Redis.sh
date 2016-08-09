bad52d7c0cdb8fb1a034d2b06e4ae0123f75ec85
Matthias Mullie <git@mullie.eu>
2016-08-09 14:13:38 +0200

    Make sure correct php-redis version gets installed

--- tests/.travis/Redis.sh
+++ tests/.travis/Redis.sh
@@ -3,6 +3,8 @@ INI_PATH=`php -r "echo php_ini_loaded_file();"`
 sudo kill -9 `sudo lsof -t -i:6379` # kill listeners on required port
 docker run -d -p 6379:6379 redis
 
+pecl uninstall redis
+
 if [[ `php-config --vernum` -ge 70000 ]] # PHP>=7.0
 then
     pecl install redis
