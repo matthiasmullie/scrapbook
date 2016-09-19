10db9983e972a1a4ddc5e1b4056121b4f54281c1
Matthias Mullie <git@mullie.eu>
2016-09-19 20:41:47 +0200

    Manually install Memcached on PHP7

--- tests/.travis/Memcached.sh
+++ tests/.travis/Memcached.sh
@@ -5,11 +5,28 @@ docker run -d -p 11211:11211 memcached
 
 if [[ $PHP_VERSION != *"hhvm" ]]
 then
-    pecl uninstall memcached
+    if [[ `php-config --vernum` -ge 70000 ]] # PHP>=7.0
+    then
+        sudo apt-get update
+        sudo apt-get install -y libmemcached-dev libmemcached11 git build-essential
 
-    # install ext-memcached requirements:
-    # "error: memcached support requires ZLIB"
-    # "error: memcached support requires libmemcached"
-    sudo apt-get -y install zlib1g-dev libmemcached-dev
-    printf "no --disable-memcached-sasl\n" | pecl install memcached
+        git clone https://github.com/php-memcached-dev/php-memcached
+        cd php-memcached
+        git checkout php7
+        git pull
+
+        /usr/local/php7/bin/phpize
+        ./configure --with-php-config=/usr/local/php7/bin/php-config
+
+        make
+        sudo make install
+    else # PHP<7.0
+        pecl uninstall memcached
+
+        # install ext-memcached requirements:
+        # "error: memcached support requires ZLIB"
+        # "error: memcached support requires libmemcached"
+        sudo apt-get -y install zlib1g-dev libmemcached-dev
+        printf "no --disable-memcached-sasl\n" | pecl install memcached
+    fi
 fi
