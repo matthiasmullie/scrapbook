5c6fcedaedbcc23f8b4ac6a623bf2c6b6fa220ec
Matthias Mullie <git@mullie.eu>
2016-12-09 11:25:55 +0100

    Fix MySQL docker port bind

--- tests/.travis/MySQL.sh
+++ tests/.travis/MySQL.sh
@@ -1,2 +1,2 @@
 sudo kill -9 `sudo lsof -t -i:3307` # kill listeners on required port
-docker run -d -p 3306:3307 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=cache mysql
+docker run -d -p 3307:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -e MYSQL_DATABASE=cache mysql
