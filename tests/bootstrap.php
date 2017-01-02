3c9b941a459ae1fa3297fb37c50b1e948b85d8d0
Matthias Mullie <git@mullie.eu>
2017-01-02 21:53:16 +0100

    Don't include cache/integration-tests by default
    cache/integration-tests needs cache/tag-interop for some
    reason. cache/tag-interop needs PHP>=5.5 for some reason.
    
    I can still install and run these tests if I ignore these
    platform requirements, but I don't want to do that (phpunit
    also depends on packages that are properly versioned and
    would install an incorrect version if I ignore platform
    requirements...)
    
    So I need to install cache/integration-tests separately.
    But the test relies on this class existing. I'll just
    create a stub if it doesn't exist.
    
    I'm getting sick and tired of this...

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,22 @@
 <?php
 
-require __DIR__.'/../vendor/autoload.php';
+namespace
+{
+    require __DIR__.'/../vendor/autoload.php';
+}
+
+namespace Cache\IntegrationTests
+{
+    if (!class_exists('Cache\IntegrationTests\CachePoolTest')) {
+        class CachePoolTest extends \PHPUnit_Framework_TestCase
+        {
+            public function testIncomplete()
+            {
+                $this->markTestIncomplete(
+                    'Missing dependencies. Please run: '.
+                    'composer require --dev cache/integration-tests:dev-master'
+                );
+            }
+        }
+    }
+}
