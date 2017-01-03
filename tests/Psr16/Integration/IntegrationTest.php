7f2c349784343ecbed840eb0ca1639b44b1bc4a8
Matthias Mullie <git@mullie.eu>
2017-01-03 18:11:44 +0100

    Add integration tests for PSR-16

---
+++ tests/Psr16/Integration/IntegrationTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MatthiasMullie\Scrapbook\Tests\Psr16\Integration;
+
+use MatthiasMullie\Scrapbook\KeyValueStore;
+use MatthiasMullie\Scrapbook\Psr16\SimpleCache;
+use MatthiasMullie\Scrapbook\Tests\AdapterTestProvider;
+use MatthiasMullie\Scrapbook\Tests\AdapterProviderTestInterface;
+
+class IntegrationPoolTest extends SimpleCacheTestCase implements AdapterProviderTestInterface
+{
+    /**
+     * @var KeyValueStore
+     */
+    protected $adapter;
+
+    /**
+     * @var string
+     */
+    protected $collectionName;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function suite()
+    {
+        $provider = new AdapterTestProvider(new static());
+
+        return $provider->getSuite();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setAdapter(KeyValueStore $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setCollectionName($name)
+    {
+        $this->collectionName = $name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function createSimpleCache()
+    {
+        return new SimpleCache($this->adapter);
+    }
+}
