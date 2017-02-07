24b903ae0a7d6ba4bbb1ed9f69662cedec3f152c
Matthias Mullie <git@mullie.eu>
2017-02-07 12:08:56 +0100

    Make sure tests don't fail when integration tests not loaded

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,8 +7,23 @@ namespace
 
 namespace Cache\IntegrationTests
 {
+    // PSR-6 integration tests
     if (!class_exists('Cache\IntegrationTests\CachePoolTest')) {
         class CachePoolTest extends \PHPUnit_Framework_TestCase
+        {
+            public function testIncomplete()
+            {
+                $this->markTestIncomplete(
+                    'Missing dependencies. Please run: '.
+                    'composer require --dev cache/integration-tests:dev-master'
+                );
+            }
+        }
+    }
+
+    // PSR-16 integration tests
+    if (!class_exists('Cache\IntegrationTests\SimpleCacheTest')) {
+        class SimpleCacheTest extends \PHPUnit_Framework_TestCase
         {
             public function testIncomplete()
             {
