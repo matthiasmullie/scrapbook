ed5396cadb13d108c4b3d7654b819e07cdafd7ae
Matthias Mullie <git@mullie.eu>
2017-02-09 12:18:56 +0100

    Update memcached setup script to reflect new versions

--- tests/.travis/Memcached.sh
+++ tests/.travis/Memcached.sh
@@ -5,31 +5,17 @@ docker run -d -p 11211:11211 memcached
 
 if [[ $PHP_VERSION != *"hhvm" ]]
 then
+    pecl uninstall memcached
+
+    # install ext-memcached requirements:
+    # "error: memcached support requires ZLIB"
+    # "error: memcached support requires libmemcached"
+    sudo apt-get -y install zlib1g-dev libmemcached-dev
+
     if [[ `php-config --vernum` -ge 70000 ]] # PHP>=7.0
     then
-        sudo apt-get update
-        sudo apt-get install -y libmemcached-dev libmemcached11 git build-essential
-
-        git clone https://github.com/php-memcached-dev/php-memcached
-        cd php-memcached
-        git checkout php7
-        git pull
-
-        phpize
-        ./configure --disable-memcached-sasl
-
-        make
-        sudo make install
-
-        INI_PATH=~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-        echo 'extension="memcached.so"' >> $INI_PATH
-    else # PHP<7.0
-        pecl uninstall memcached
-
-        # install ext-memcached requirements:
-        # "error: memcached support requires ZLIB"
-        # "error: memcached support requires libmemcached"
-        sudo apt-get -y install zlib1g-dev libmemcached-dev
         printf "no --disable-memcached-sasl\n" | pecl install memcached
+    else # PHP<7.0
+        printf "no --disable-memcached-sasl\n" | pecl install memcached-2.2.0
     fi
 fi
