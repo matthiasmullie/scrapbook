ad98e2be45c4394c95c8237b9e6b22bb2fe8a65b
Matthias Mullie <git@mullie.eu>
2017-09-26 16:42:15 +0200

    Conditionally install APC/APCu, depending on PHP version

--- tests/Docker/build-php.sh
+++ tests/Docker/build-php.sh
@@ -1,24 +1,43 @@
-#!/bin/sh
+#!/bin/bash
 
 curl -sS http://packages.couchbase.com/ubuntu/couchbase.key | apt-key add -
 curl -sS -o /etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com/ubuntu/couchbase-ubuntu1404.list
 apt-get update
 
-# install PHP extensions
 apt-get install -y libcouchbase2-libevent libcouchbase-dev libmemcached-dev zlib1g-dev libpq-dev
-pecl install -f apc apcu pcs-1.3.3 igbinary couchbase memcached redis
-docker-php-ext-enable apc # PHP 5.*
-docker-php-ext-enable apcu # PHP 7.*
+
+# install PHP extensions
+if [[ `php-config --vernum` -ge 70000 ]]; then # PHP>=7.0
+    pecl install -f apcu
+    docker-php-ext-enable apcu
+else # PHP<7.0
+    pecl install -f apc
+    docker-php-ext-enable apc
+fi
+echo "apc.enable_cli=1" >> /usr/local/etc/php/php.ini
+
+pecl install -f pcs-1.3.3
 docker-php-ext-enable pcs
+
+pecl install -f igbinary
 docker-php-ext-enable igbinary
+
+pecl install -f couchbase
 docker-php-ext-enable couchbase
+
+pecl install -f memcached
 docker-php-ext-enable memcached
+
+pecl install -f redis
 docker-php-ext-enable redis
+
 docker-php-ext-install pdo
+
 docker-php-ext-install pdo_mysql
+
 docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql
 docker-php-ext-install pdo_pgsql
-echo "apc.enable_cli=1" >> /usr/local/etc/php/php.ini
+
 rm -rf /tmp/pear
 
 # install composer and a bunch of dependencies
