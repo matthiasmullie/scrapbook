93068e938b4646b60abb6fe05f66bf9550a20a92
Matthias Mullie <git@mullie.eu>
2017-10-06 12:42:13 +0200

    Install older packages when <7.0

--- tests/Docker/build-php.sh
+++ tests/Docker/build-php.sh
@@ -7,34 +7,29 @@ apt-get update
 apt-get install -y libcouchbase2-libevent libcouchbase-dev libmemcached-dev zlib1g-dev libpq-dev
 
 # install PHP extensions
+pecl install -f pcs-1.3.3
+pecl install -f igbinary
+pecl install -f couchbase
+
 if [[ `php-config --vernum` -ge 70000 ]]; then # PHP>=7.0
     pecl install -f apcu
-    docker-php-ext-enable apcu
+    pecl install -f memcached
+    pecl install -f redis
 else # PHP<7.0
-    pecl install -f apc
-    docker-php-ext-enable apc
+    pecl install -f apcu-4.0.10
+    pecl install -f memcached-2.2.0
+    pecl install -f redis-2.2.7
 fi
+
+docker-php-ext-enable apcu
 echo "apc.enable_cli=1" >> /usr/local/etc/php/php.ini
-
-pecl install -f pcs-1.3.3
 docker-php-ext-enable pcs
-
-pecl install -f igbinary
 docker-php-ext-enable igbinary
-
-pecl install -f couchbase
 docker-php-ext-enable couchbase
-
-pecl install -f memcached
 docker-php-ext-enable memcached
-
-pecl install -f redis
 docker-php-ext-enable redis
-
 docker-php-ext-install pdo
-
 docker-php-ext-install pdo_mysql
-
 docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql
 docker-php-ext-install pdo_pgsql
 
